4ee2b4bcd8a3e61e73213b48a898074ed11d919f
r-kanai-bit <r-kanai@harmony-mie.com>
2026-02-23 20:41:05 +0900

    Connect FastAPI to PostgreSQL database

--- main.py
+++ main.py
@@ -1,7 +1,18 @@
 from fastapi import FastAPI
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from models import Base
+import os
+
+DATABASE_URL = os.getenv("DATABASE_URL")
+
+engine = create_engine(DATABASE_URL)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 
 @app.get("/")
 def root():
-    return {"status": "API running"}
+    return {"status": "Postgres connected"}
